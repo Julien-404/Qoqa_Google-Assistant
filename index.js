f3a310b9f521334c2f9e4a6973034edec6371c8b
Julien Dubi <julien.dubi13@gmail.com>
2018-04-18 01:26:43 +0200

    heroku fixing shit

--- index.js
+++ index.js
@@ -62,5 +62,11 @@ app.on('error', (err, ctx) => {
     console.log('error in function :', err);
   });
 
-process.env.PORT || 3000;
-app.listen(3000);
+  app.set('port', (process.env.PORT || 3000))
+  app.listen(app.get('port'), function() {
+    console.log("Node app is running at localhost:" + app.get('port'))
+  })
+
+
+// process.env.PORT || 3000;
+// app.listen(3000);
