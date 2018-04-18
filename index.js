0e813ab98e4af14072b2dc30dec89e541285f1e7
Julien Dubi <julien.dubi13@gmail.com>
2018-04-18 11:02:14 +0200

    one more try

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ app.use(async ctx => {
     await getOffer(offer).then((output) => {
         console.log(output);
         ctx.set('Content-Type', 'application/json; charset=UTF-8');
-        ctx.body = JSON.stringify({'responseId': responseID, 'data': {'google': {'richResponse': {'items': [ {'simpleResponse':  { 'textToSpeech': output, 'displayText': output }}]}}}});
+        ctx.body = JSON.stringify({'data': {'google': {'richResponse': {'items': [ {'simpleResponse':  { 'textToSpeech': output, 'displayText': output }}]}}}});
     }).catch((error) => {
         ctx.set('Content-Type', 'application/json; charset=UTF-8');
         ctx.body = JSON.stringify({ 'speech': error, 'displayText': error });
